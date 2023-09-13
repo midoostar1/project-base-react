6433883662a9fbf5c8b15dc5101f66a4d9f863db
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2023-09-13 00:10:39 -0500

    splitting components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import './expenseItem.css'
+import React from "react";
+import "./expenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem({title, date, amount}) {
-    
+//destructured props in the function declaration.
+function ExpenseItem({ title, date, amount }) {
   return (
-    <div className='expense-item'>
-     <div className="expense-item__description">
-<div>{date.toISOString()}</div>
-     </div>
-<h2>{title}</h2>
-<div className="expense-item__price">${amount}</div>
+    <div className="expense-item">
+        <ExpenseDate date = {date}/>
+      <div className="expense-item__description"></div>
+      <h2>{title}</h2>
+      <div className="expense-item__price">${amount}</div>
     </div>
-  )
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
